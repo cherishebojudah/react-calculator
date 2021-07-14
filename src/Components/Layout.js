05a8500210984b8c59b00d210dae164d790a3486
Cherish Collins <cherish.ebojudah@gmail.com>
2021-07-14 22:43:04 +0100

    added input buttons for calculator

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,8 +2,42 @@ import React from 'react';
 import './Layout.css';
 
 const Layout = () => {
+    const HandleClick = () => {}
+
     return (
-        <div>Say hello</div>
+        <div className='frame'>
+            <div className='calculator'>
+            <br></br>
+
+            <div className='keys'>
+                <input type='button' value={'C'} className='button clear' onClick={HandleClick}></input>
+                <input type='button' value={'DEL'} className='button clear' onClick={HandleClick}></input>
+                <input type='button' value={'%'} className='button operator' onClick={HandleClick}></input>
+                <input type='button' value={'/'} className='button operator' onClick={HandleClick}></input>
+
+                <input type='button' value={'7'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'8'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'9'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'*'} className='button operator' onClick={HandleClick}></input>
+
+                <input type='button' value={'4'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'5'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'6'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'-'} className='button operator' onClick={HandleClick}></input>
+
+                <input type='button' value={'1'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'2'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'3'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'+'} className='button operator' onClick={HandleClick}></input>
+
+                <input type='button' value={'0'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'.'} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={''} className='button ' onClick={HandleClick}></input>
+                <input type='button' value={'='} className='button equal-sign' onClick={HandleClick}></input>
+            </div>
+            
+            </div>
+        </div>
     )
 }
 
