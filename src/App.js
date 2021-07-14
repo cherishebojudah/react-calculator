ba8f1e042ff437861c6fab3aa2a687548d43c878
Cherish Collins <cherish.ebojudah@gmail.com>
2021-07-14 22:29:45 +0100

    created components for calculator layout and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from './Components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Layout></Layout>
     </div>
   );
 }
---
+++ src/Components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Layout.css';
+
+const Layout = () => {
+    return (
+        <div>Say hello</div>
+    )
+}
+
+export default Layout;
